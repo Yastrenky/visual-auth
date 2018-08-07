6821b61c14b915cbeb0280c117055c783155c1bf
Yastrenky <ybramos2014@gmail.com>
2018-08-06 18:00:55 -0700

    Delete config.js

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-var serverhost = 'http://localhost:8001';
-var secure = 'mybelovedsecretshhh';
-var recaptcha = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
-export {secure , recaptcha }
-export default serverhost;
