02ed24c1d197415a6ca5e7ae7c210ab4391111ac
Yastrenky <ybramos2014@gmail.com>
2018-08-07 15:46:38 -0700

    Delete config.js

--- src/config/config.js
+++
@@ -1,6 +0,0 @@
-var serverhost = 'http://localhost:8001';
-var secure = 'mybelovedsecretshhh';
-var recaptcha = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
-var stripekey = 'pk_test_ZXKhepp6s1snDEZSp03vqRWT';
-export { secure, recaptcha, stripekey }
-export default serverhost;
