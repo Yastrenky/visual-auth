920d6ebfa313b4eda833bf406710346d2a021dd1
Yastrenky <ybramos2014@gmail.com>
2022-03-26 01:33:52 -0400

    git ignore fix, and package.json conflic fix

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const stripekey = "pk_test_ZXKhepp6s1snDEZSp03vqRWT"
+export const recaptcha = "6LcsqGgUAAAAAA8KnEpjSTTBN1GNJepRjWMayCF1"
+export const server="http://localhost:8001"
+export const secure = "8a4ec1eadb33970e44fda21f438aad30af"
